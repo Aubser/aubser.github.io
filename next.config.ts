fb53d37f81487bb14665f7969d2a59e33c1495f4
AubreySwen <auswenson1@gmail.com>
2025-05-22 18:33:54 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   basePath: "/aubser.github.io",
    output: "export",  // <=== enables static exports
    reactStrictMode: true,
 };
