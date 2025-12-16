bea437bd0594694eba18ba376efaa59d97179b2d
AubreySwen <auswenson1@gmail.com>
2025-12-15 19:15:32 -0600

    Replace drone pic

--- src/components/featuresHorizontalBoxes.tsx
+++ src/components/featuresHorizontalBoxes.tsx
@@ -10,7 +10,7 @@ export default function FeaturesHorizontalBoxes() {
                      <p>High-speed, long-range drones engineered for endurance and precision.</p>
                   </div>
                   <div className="relative bg-gray-50 p-6 rounded-lg" style={{boxShadow: '0 4px 15px rgba(25, 154, 72, 0.4)'}}>
-                     <img src="/basicDesign.png" alt="Innovative Designs" className="rounded-full object-cover w-50 h-50 mx-auto mb-4" />
+                     <img src="/Innovative.jpg" alt="Innovative Designs" className="rounded-full object-cover w-50 h-50 mx-auto mb-4" />
                      <h3 className="text-xl font-semibold mb-2">Innovative Designs</h3>
                      <p>Crafting innovative designs that push the boundaries of technology and elevate it to its full potential.</p>
                   </div>
